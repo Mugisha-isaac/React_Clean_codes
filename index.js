affd8fe1123d64004f755a9fc8ed8ac38094321d
mugishaisaac0508@gmail.com <mugishaisaac0508@gmail.com>
2023-02-08 11:03:31 +0200

    [feat:index.js] converting imperative codes into declarative codes

---
+++ index.js
@@ -0,0 +1,21 @@
+
+
+// writting a function to return strings uppercased from the array
+
+// imperative programming
+
+// const toUpper = input=>{
+//     let output = [];
+//     for(let i=0;i<input.length;i++){
+//         output.push(input[i].toUpperCase());
+//     }
+//     return output;
+// }
+
+// console.log(toUpper(['hello','isaac']));
+
+
+// declarative programming
+
+// const toUpper = input => input.map(value=>value.toUpperCase());
+// console.log(toUpper(['hello','isaac']));
